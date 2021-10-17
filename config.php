625fc3894649a745c315d3c1c3c8a4da95a0b600
Matt Nguyen <newyenmatt@gmail.com>
2021-10-17 12:27:29 -0500

    First comment

---
+++ config.php
@@ -0,0 +1,23 @@
+<!-- 
+	Insert base of the URL (home directory) to be placed into the href 
+	By echoing in href, it displays in the URL
+	Place this php at the top <head>
+-->
+<?php
+	//$base = "https://www.chicagoshufflers.com";
+	$base = "http://localhost/chicagoshufflers";
+	$date = date("D M d, Y G:i"); // For versioning
+?>
+<!-- Tab icon -->
+<link rel = "icon" href = "<?php echo $base;?>/images/logos/CS_Logo_02.jpg">
+
+<!-- Main files -->
+<link type = "text/css" rel = "stylesheet" href = "<?php echo $base;?>/styles.css?v=<?php echo $date;?>">
+<script type = "text/javascript" src = "<?php echo $base;?>/script.js?v=<?php echo $date;?>"></script>
+
+<!-- Shortcut Icon + Scalable view -->
+<!--<link rel = "shortcut icon" href = "./images/logos/CS_Logo_02.jpg">-->
+<meta name="viewport" content="height=device-height, 
+    					width=device-width, initial-scale=1.0, 
+                      	minimum-scale=1.0, maximum-scale=1.0, 
+                      	user-scalable=no">
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head> 
+	<!-- Defaults, CSS, JS main files -->
+	<?php include('config.php'); ?>
+	<title> Chicago Shufflers </title>
+</head>
+<body>
+	<div class = "main-box">
+		<div class = "line-right-to-left">
+
+		</div>
+	</div>
+
+	<div class = "main-box">
+
+	</div>
+
+
+</body>
+</html>
