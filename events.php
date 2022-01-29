8156084c594f06f84feaae5f11e06aac21d6e29d
Matt Nguyen <newyenmatt@gmail.com>
2022-01-28 23:43:05 -0600

    Stuff

--- events.php
+++ events.php
@@ -19,7 +19,18 @@
 		===========================================================
 	-->
 	<div id = "main-container" class = "main-container">
-		
+		<div class = "timeline">
+			<div class = "timeline-left-container">
+				<div class = "timeline-left-content">
+					<h1> Grant Park Meetup</h1>
+					<h2> 05/02/21 </h2>
+					<p1> Lorem ipsum dolor sit amet, quo ei simul congue exerci, ad nec admodum perfecto mnesarchum, vim ea mazim fierent detracto. Ea quis iuvaret expetendis his, te elit voluptua dignissim per, habeo iusto primis ea eam. </p1> 
+				</div>
+			</div>
+
+			<div class = "timeline-right-container">
+			</div>
+		</div>
 	</div> <!-- End of main container -->
 
 
